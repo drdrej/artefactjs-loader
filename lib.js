edecaac308b06dee38ac78e4c6608b491d116b6c
drdrej <andrej.siebert@googlemail.com>
2014-07-15 16:26:21 +0200

    impl load(fnc), refactoring

--- lib.js
+++ lib.js
@@ -1,96 +1,37 @@
 var fs = require('fs');
-var colors = require( 'colors' );
+require( 'colors' );
 
+var use = function( type, path, strategy ) {
+    var load = require( './loader/' + type).load;
+    var loader = require( './loader/loader').loader;
 
-var loadJSON = function( path ) {
-    var json = null;
-    try {
-        json = require(path);
-    } catch(err){
-        console.log( "[ERROR] couldn't load config. check exception...".red );
-        console.log( err );
+    return loader(path, load, strategy);
+};
 
-        return;
+/**
+ * load path.
+ *
+ * @param path
+ */
+exports.json = function( path, strategy ) {
+    if( !strategy ) {
+        strategy = function (path) {
+            return require(path);
+        };
     }
 
-    if( !json ) {
-        console.log( "[ERROR] couldn't load config. something is wrong...".red );
+    return use( 'json', path, strategy );
+};
 
-        return;
+exports.fnc = function( path, strategy ) {
+
+    if( !strategy ) {
+        strategy = function (path) {
+            return require(path);
+        };
     }
 
-    console.log( ("[INFO] json loaded. path = " + path).green);
-
-    var stats = fs.statSync(path);
-
-    return {
-        "$path" : path,
-        "$modified" :stats.mtime,
-        "$json" : json
-    };
+    return use( 'fnc', path, strategy);
 };
 
 
-var loadInProcess = function( path ) {
-    var Path = require( 'path' );
-
-    console.log( "-- in caller-path not exists, try in process..." );
-    var inProcess = Path.resolve( process.cwd(), path);
-
-    if( fs.existsSync(inProcess) ) {
-        return loadJSON( inProcess );
-    }
-};
-
-var loadInCaller = function(path) {
-    var callsite = require('callsite');
-
-    var stack = callsite();
-    var callerFile = stack[2].getFileName();
-
-    var Path = require( 'path' );
-    var callerDir = Path.dirname( callerFile );
-    var resolved = Path.resolve(callerDir, path);
-
-    console.log( "-- use caller-file to resolve json-path." );
-    console.log( " try: " + resolved );
-
-    if( fs.existsSync(resolved) ) {
-        return loadJSON(resolved);
-    }
-};
-
-
-exports.load = function( path ) {
-    var S = require( 'string' );
-    var str = S(path);
-
-    if( !str.endsWith( '.json' ) ) {
-        console.log( "[ERROR] couldn't load config. path should ends with '.json'.".red);
-        console.log( "-- passed path:  " + path );
-
-        return;
-    }
-
-
-    if( !fs.existsSync(path) ) {
-        console.log( "-- path not exists. path : " + path );
-        console.log( "-- try to resolve... " );
-
-        var loaded = loadInProcess(path);
-        if( loaded )
-            return loaded;
-
-        loaded = loadInCaller(path);
-        if( loaded )
-            return loaded;
-
-        console.log( "[ERROR] couldn't resolve file: " + path);
-        return;
-    } else {
-        console.log( "-- file exists: " + path);
-        return loadInProcess(path);
-    }
-
-    return loadJSON( path );
-};
